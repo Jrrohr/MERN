b83ac5fbe34fec54cec312c72c950281b33a307f
Jrrohr <jbanzet17@gmail.com>
2023-03-08 20:43:54 -0800

    first commit

---
+++ week1/desctructuring.js
@@ -0,0 +1,6 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//I predict Tesla and then Mercedes to be printed.
+console.log(randomCar)
+console.log(otherRandomCar)
