4748f116e54da640963d60f48f62f11903f70aa1
Jrrohr <jbanzet17@gmail.com>
2023-04-12 16:48:16 -0700

    Project manager assignment pt 1

---
+++ week6/projectManager/client/src/components/ProductForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+const ProductForm = () => {
+    const [title, setTitle] = useState(""); 
+    const [price, setPrice] = useState(0.0);
+    const [description, setDescription] = useState("");
+
+    //handler when the form is submitted
+    const onSubmitHandler = (e) => {
+        //prevent default behavior of the submit
+        e.preventDefault();
+        //make a post request to create a new product
+        axios.post('http://localhost:8000/api/products', {
+            title,
+            price,
+            description
+        })
+            .then(res=>{
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(err=>console.log(err))
+    }
+
+    const formStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '15px',
+        backgroundColor: 'rebeccapurple',
+        border: '1px darkgray solid',
+        color: 'white',
+        borderRadius: '5px'
+    }
+    
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <h2>Product Manager</h2>
+            <div style={{ width: "350px", margin: "auto"}}>
+            <p style={formStyle}>
+                <label>Title: </label>
+                <input type="text" onChange = {(e)=>setTitle(e.target.value)}/>
+            </p>
+            <p style={formStyle}>
+                <label>Price: </label>
+                <input type="number" step="any" onChange = {(e)=>setPrice(e.target.value)}/>
+            </p>
+            <p style={formStyle}>
+                <label>Description: </label>
+                <input type="text" onChange = {(e)=>setDescription(e.target.value)}/>
+            </p>
+            <input type="submit" value="Create"/>
+            </div>
+        </form>
+    )
+}
+export default ProductForm;
---
+++ week6/projectManager/server/controllers/product.controller.js
@@ -0,0 +1,13 @@
+const Product = require('../models/product.model');
+
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+
+module.exports.createProduct = (request, response) => {
+    Product.create(request.body) //This will use whatever the body of the client's request sends over
+        .then(product => response.json(product))
+        .catch(err => response.json(err));
+}
