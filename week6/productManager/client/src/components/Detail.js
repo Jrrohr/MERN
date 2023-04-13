400d687981968cfbb3f02e23e5715dd2597b5f82
Jrrohr <jbanzet17@gmail.com>
2023-04-13 08:23:55 -0700

    Added delete button to detail page

--- week6/productManager/client/src/components/Detail.js
+++ week6/productManager/client/src/components/Detail.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import {useParams, Link} from "react-router-dom";
+import { useNavigate, useParams, Link } from "react-router-dom";
 const Detail = (props) => {
     const [product, setProduct] = useState({})
     const {id} = useParams(); 
+    const navigate = useNavigate();
     useEffect(() => {
         axios.get("http://localhost:8000/api/products/" + id)
             .then( res => {
@@ -13,11 +14,22 @@ const Detail = (props) => {
             .catch( err => console.log(err) );
     }, []);
 
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/products/' + productId)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                navigate("/home");
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div>
             <p>Title: {product.title}</p>
             <p>Price: ${Number(product.price).toFixed(2)}</p>
             <p>Description: {product.description}</p>
+            <p><button onClick={(e)=>{deleteProduct(product._id)}}>Delete</button></p>
             <Link to={`/home`}>Back</Link>
         </div>
     );
